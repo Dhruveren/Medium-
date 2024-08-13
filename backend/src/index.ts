01044087621120062953564c52ffa5e5e034e0a3
Debasish Sarkar <trader2004jpg@gmail.com>
2024-08-13 19:18:40 +0530

    Added fe

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
 import { userRouter } from './routes/user'
 import { blogRouter } from './routes/blog'
+import { cors } from 'hono/cors'
+
 
 
 
@@ -14,6 +16,7 @@ const app = new Hono<{
     JWT_SECRET: string;
   }
 }>()
+app.use('/*', cors());
 
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/blog", blogRouter);
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -99,7 +99,18 @@ blogRouter.put('/', async (c) => {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
       }).$extends(withAccelerate())
-      const blogs = await prisma.blog.findMany();
+      const blogs = await prisma.blog.findMany({
+        select: {
+            content: true,
+            title: true,
+            id: true ,
+            author: {
+                select: {
+                    name: true
+                }
+            }
+        }
+      });
 
       return c.json({
           blogs
@@ -117,6 +128,16 @@ blogRouter.get('/:id', async (c) => {
             where: {
                 id: Number(id)
             },
+            select: {
+                id: true,
+                title: true,
+                content: true,
+                author: {
+                    select: {
+                        name: true 
+                    }
+                }
+            }
             
         })
     
